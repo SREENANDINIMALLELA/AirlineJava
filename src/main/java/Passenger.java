449137facd40aca667e37536c951d2c3ce060b86
sreenandini <mallelasreenandini2@gmail.com>
2019-11-10 12:30:15 +0000

    added passengers seat number

--- src/main/java/Passenger.java
+++ src/main/java/Passenger.java
@@ -1,10 +1,12 @@
 public class Passenger {
     private String name ;
     private int numberOfBags;
+    private Flight bookedFlight;
+    private int seatNumber;
     public Passenger(String name , int numberOfBags) {
         this.name= name ;
         this.numberOfBags=numberOfBags;
-
+        this.bookedFlight=null;
     }
 
     public String getName() {
@@ -14,4 +16,19 @@ public class Passenger {
         return numberOfBags;
     }
 
+    public void setBookedFlight(Flight bookedFlight) {
+        this.bookedFlight = bookedFlight;
+    }
+
+    public String getflightNumber() {
+      return  bookedFlight.getflightNumber();
+    }
+
+    public int getSeatNumber() {
+        return seatNumber;
+    }
+
+    public void setSeatNumber(int seatNumber) {
+        this.seatNumber = seatNumber;
+    }
 }
--- src/test/java/PassengerTest.java
+++ src/test/java/PassengerTest.java
@@ -1,22 +1,43 @@
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.Calendar;
+
 import static junit.framework.TestCase.assertEquals;
 
 public class PassengerTest {
-    Passenger passenger;
+    private  Passenger passenger1;
+    private  Flight flight;
+    private Plane plane ;
+    private ArrayList<Passenger> passengers = new ArrayList<Passenger>();
     @Before
-    public void dummy() {
-        passenger = new Passenger("Sree",2);
+    public void before(){
+        plane = new Plane(PlaneType.BOEING737);
+        passenger1= new Passenger("Sree",1);
+        flight = new Flight(plane ,"BA123","Glagow","Hyderabad", Calendar.getInstance().getTime());
     }
 
     @Test
     public void nameForPassengerTest(){
-        assertEquals("Sree",passenger.getName());
+        assertEquals("Sree",passenger1.getName());
     }
     @Test
     public void bagsForPassengerTest(){
-        assertEquals(2,passenger.getNumberOfBags());
+        assertEquals(1,passenger1.getNumberOfBags());
     }
+    @Test
 
+    public void  getPassengerFlightTest() {
+        passengers.add(passenger1);
+        flight.addBooking(passengers);
+        assertEquals("BA123", passenger1.getflightNumber());
+    }
+    @Test
+
+    public void  getPassengerSeatNumber() {
+        passengers.add(passenger1);
+        flight.addBooking(passengers);
+        System.out.println( passenger1.getSeatNumber());
+    }
 }
